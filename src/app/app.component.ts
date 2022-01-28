eca3919bd4f99046e923130cf1eb85cd9157033a
Jonas Wolfram <gartenspaten@web.de>
2022-01-28 17:18:12 +0100

    Created hero and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'thai-rin-ruam-new';
+
+  ngOnInit(): void {
+    // Default theme is light
+    document.documentElement.classList.add('light-theme');
+  }
 }
--- src/app/features/user-space/user-space.module.ts
+++ src/app/features/user-space/user-space.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserSpaceRoutingModule } from './user-space-routing.module';
-
+import { HomeComponent } from './pages/home/home.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { OpeningHoursComponent } from './components/opening-hours/opening-hours.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    UserSpaceRoutingModule
-  ]
+  declarations: [HomeComponent, HeroComponent, OpeningHoursComponent, HeaderComponent],
+  imports: [CommonModule, UserSpaceRoutingModule, SharedModule],
 })
-export class UserSpaceModule { }
+export class UserSpaceModule {}
