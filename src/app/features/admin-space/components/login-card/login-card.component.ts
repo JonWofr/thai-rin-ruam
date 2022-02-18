d582f4131270b97e0ae556c079d7388deef428db
Jonas Wolfram <gartenspaten@web.de>
2022-02-18 13:38:43 +0700

    Implemented authentication

--- src/app/features/admin-space/components/login-card/login-card.component.ts
+++ src/app/features/admin-space/components/login-card/login-card.component.ts
@@ -1,9 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ToastType } from '../../enums/toast-type.enum';
-import { SpinnerControllerService } from '../../services/spinner-controller/spinner-controller.service';
-import { ToastControllerService } from '../../services/toast-controller/toast-controller.service';
 
 @Component({
   selector: 'admin-space-login-card',
@@ -11,23 +7,9 @@ import { ToastControllerService } from '../../services/toast-controller/toast-co
   styleUrls: ['./login-card.component.scss'],
 })
 export class LoginCardComponent implements OnInit {
-  constructor(
-    private toastController: ToastControllerService,
-    private spinnerController: SpinnerControllerService,
-    private router: Router
-  ) {}
+  @Output() submitLoginForm = new EventEmitter<NgForm>();
+
+  constructor() {}
 
   ngOnInit(): void {}
-
-  onSubmit(form: NgForm) {
-    this.spinnerController.subject.next(true);
-    setTimeout(() => {
-      this.spinnerController.subject.next(false);
-      this.toastController.subject.next({
-        type: ToastType.SUCCESS,
-        message: 'Erfolgreich eingelogt',
-      });
-      this.router.navigateByUrl('/admin');
-    }, 1000);
-  }
 }
